528f7785a9ca20a43e82cbfc34b03692c599761d
Ubuntu <ubuntu@ip-10-0-13-141.ap-northeast-1.compute.internal>
2024-12-23 05:41:16 +0000

    dags upload

---
+++ dags/dag_get_current_location.py
@@ -0,0 +1,112 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+import requests
+import pandas as pd
+from io import StringIO
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+from datetime import datetime, timedelta
+
+
+# 인증 정보
+OpenskyID = "nyoths1379"
+OpenskyPW = "tara7350" 
+SnowflakeID = "bibibig"
+SnowflakePW = "Bingrebibibic12!"
+SnowflakeAccount = "cdbccwb-ae90733"
+SnowflakeDatabase = "BIBIBIG"
+SnowflakeSchema = "BRONZE_STATE_DATA"
+
+#%%
+# 모든 위치 상태값 검색 -> 호출 제한량 있음, 
+# 일일 4000 크레딧 보유
+# all 요청 시 4 크레딧 필요 ()
+# 제한에 도달한 후에는 상태 코드 429 - Too Many Requests가 반환
+# 헤더 X-Rate-Limit-Retry-After-Seconds는 크레딧/요청이 다시 사용 가능해질 때까지 몇 초가 걸리는지 정보 제공
+
+
+def get_airplane_location(**kwargs):
+    url = 'https://opensky-network.org/api/states/all'
+    response = requests.get(url, auth=(OpenskyID, OpenskyPW))
+
+    # 응답 확인
+    data = response.json()
+
+    df_all_state_vector = pd.DataFrame(data['states'])
+
+    df_all_state_vector.columns =  ['icao24','callsign','origin_country',
+                                    'time_position','last_contact','longitude',
+                                    'latitude','baro_altitude','on_ground',
+                                    'velocity','true_track','vertical_rate',
+                                    'sensors','geo_altitude','squawk','spi',
+                                    'position_source'] # ,'category']
+    
+    df_all_state_vector['createAt'] = datetime.fromtimestamp(data['time'])
+    
+
+    return df_all_state_vector
+
+
+# S3 업로드 함수
+def upload_to_snowflake(conn_info, dataframe, table_name):  
+    with snowflake.connector.connect(user=conn_info['user'], 
+                                     password=conn_info['password'], 
+                                     account=conn_info['account'],
+                                     database=conn_info['database'],
+                                     schema=conn_info['schema']) as conn:
+        write_pandas(conn, dataframe, table_name)
+
+
+
+def put_airplane_data(**kwargs):
+    # XCom에서 데이터프레임 받기
+    ti = kwargs['ti']
+    df = ti.xcom_pull(task_ids='get_airplane_location')
+    
+    conn_info = {'user' : SnowflakeID,
+                 'password' : SnowflakePW,
+                 'account' : SnowflakeAccount,
+                 'database' : SnowflakeDatabase,
+                 'schema' : SnowflakeSchema}
+    
+    # snowflake only used capital letter colname
+    df.columns = [col.upper() for col in df.columns]
+    upload_to_snowflake(conn_info, df, 'AIRPLANE_LOCATION')
+    
+    
+
+# DAG 정의
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG(
+    'upload_location_data',
+    default_args=default_args,
+    description='Fetch API data, process into DataFrame, and upload to snowflake',
+    schedule_interval=timedelta(minutes=2),
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+) as dag:
+
+    fetch_and_process_data_task = PythonOperator(
+        task_id = 'fetch_and_process_data',
+        python_callable = get_airplane_location,
+        provide_context = True
+    )
+
+    upload_partitioned_data_task = PythonOperator(
+        task_id = 'upload_partitioned_data',
+        python_callable = put_airplane_data,
+        provide_context = True
+    )
+
+    fetch_and_process_data_task >> upload_partitioned_data_task
+    
